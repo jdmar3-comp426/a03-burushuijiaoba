e6b70f758e08f74c68067e028a3e28d318613b66
Ryanyijia <ryanyijia@gmail.com>
2021-11-10 23:50:56 -0500

    22

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import { getSum } from './src/medium/medium_1.js';
 import {multiplyBy, repeat, repeatDemo} from './src/spicy/spicy_9.js'
 
-console.log(repeat(console.log,10,5));
+console.log(repeatDemo());
